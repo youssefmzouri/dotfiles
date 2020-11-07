b90dbaffd656086d0fb525143047556509b5c993
Youssef <youssefmzouri@gmail.com>
2020-11-07 01:48:16 +0100

    ordered in files repo and added script to create symlinks

---
+++ symlinks/links.sh
@@ -0,0 +1,4 @@
+ln -s "$HOME/.dotfiles/shell/bash/.bash_profile" "$HOME/.bash_profile"
+ln -s "$HOME/.dotfiles/shell/zsh/.zshrc" "$HOME/.zshrc"
+ln -s "$HOME/.dotfiles/git/.gitconfig" "$HOME/.gitconfig"
+ln -s "$HOME/.dotfiles/git/.gitignore_global" "$HOME/.gitignore_global"
